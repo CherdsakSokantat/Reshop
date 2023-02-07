bd65df01391f55c1143661fde7e564996cda43a1
CherdsakSokantat <cherdsakso@kkumail.com>
2023-02-07 11:06:54 +0700

    Init the project

---
+++ client/src/components/create-student.component.jsx
@@ -0,0 +1,43 @@
+// CreateStudent Component for add new student
+
+// Import Modules
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import StudentForm from "./StudentForm";
+import Swal from 'sweetalert2';
+// CreateStudent Component
+const CreateStudent = () => {
+const [formValues, setFormValues] =
+	useState({ name: '', email: '', rollno: '' })
+// onSubmit handler
+const onSubmit = studentObject => {
+	axios.post(
+'http://localhost:4000/students/create-student',
+	studentObject)
+	.then(res => {
+		if (res.status === 200)
+		Swal.fire({
+			icon: "success",
+			text: "success"
+		})
+		else
+		Promise.reject()
+	})
+	.catch(err => Swal.fire({
+		text: "error somthing when wrong",
+		icon: "error"
+	}))
+}
+	
+// Return student form
+return(
+	<StudentForm initialValues={formValues}
+	onSubmit={onSubmit}
+	enableReinitialize>
+	Create Student
+	</StudentForm>
+)
+}
+
+// Export CreateStudent Component
+export default CreateStudent
